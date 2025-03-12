a61231b8c9e5eb8d7e551262d534a46d357232a7
Kate Jang <bobaejang1@gmail.com>
2025-03-12 15:01:10 +0900

    storybook dependencies downloaded

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import type { StorybookConfig } from '@storybook/experimental-nextjs-vite';
+=======
+import type { StorybookConfig } from '@storybook/nextjs';
+>>>>>>> c56fa985 (storybook dependencies downloaded)
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -8,10 +12,26 @@ const config: StorybookConfig = {
     '@chromatic-com/storybook',
     '@storybook/experimental-addon-test',
   ],
+<<<<<<< HEAD
   framework: {
     name: '@storybook/experimental-nextjs-vite',
     options: {},
   },
   staticDirs: ['..\\public'],
+=======
+  "addons": [
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@chromatic-com/storybook",
+    "@storybook/experimental-addon-test"
+  ],
+  "framework": {
+    "name": "@storybook/nextjs",
+    "options": {}
+  },
+  "staticDirs": [
+    "..\\public"
+  ]
+>>>>>>> c56fa985 (storybook dependencies downloaded)
 };
 export default config;

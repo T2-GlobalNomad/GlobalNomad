d12da13494e2e236f01840d97a9e1bac52fb3b7b
이승환 <ls991217@naver.com>
2025-03-13 23:09:33 +0900

    feat: 카테고리 목록 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,16 +12,25 @@ export default function Home() {
     <>
       <div className={styles.imgContainer}>
         <div className={styles.textContainer}>
-          <p className={styles.text}>
+          <p className={styles.text1}>
             함께 배우면 즐거운
             <br /> 스트릿 댄스
           </p>
-          <p>1월의 인기체험 BEST</p>
+          <p className={styles.text2}>1월의 인기체험 BEST</p>
         </div>
       </div>
 
-      {/* main영역 */}
-      <div className={styles.mainContainer}>
+      {/* 카테고리 영역 */}
+      <div className={styles.categoryContainer}>
+        <ul className={styles.category}>
+          <li className={styles.item}>문화•예술</li>
+          <li className={styles.item}>식음료</li>
+          <li className={styles.item}>스포츠</li>
+          <li className={styles.item}>투어</li>
+          <li className={styles.item}>관광</li>
+          <li className={styles.item}>웰빙</li>
+        </ul>
+
         <Dropdown
           options={['최신순', '낮은가격순', '높은가격순']}
           selected={selectedSort}

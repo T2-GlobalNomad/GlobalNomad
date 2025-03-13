be25d9821a9eedb8aea00827780bb8c23f62eafc
이승환 <ls991217@naver.com>
2025-03-13 17:23:30 +0900

    feat: 랜딩페이지 상단 이미지 작성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,34 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import Dropdown from "@/components/Dropdown";
-// import styles from "./LandingPage.module.css";
+import { useState } from 'react';
+import Dropdown from '@/components/Dropdown';
+// import Footer from '@/components/footer/Footer';
+import styles from './LandingPage.module.css';
 
 export default function Home() {
-  const [selectedSort, setSelectedSort] = useState("최신순");
+  const [selectedSort, setSelectedSort] = useState('최신순');
 
   return (
     <>
-      <p>Landing(main)</p>
-      <Dropdown
-        options={["최신순", "낮은가격순", "높은가격순"]}
-        selected={selectedSort}
-        onChange={setSelectedSort}
-      />
+      <div className={styles.imgContainer}>
+        <div className={styles.textContainer}>
+          <p className={styles.text}>
+            함께 배우면 즐거운
+            <br /> 스트릿 댄스
+          </p>
+          <p>1월의 인기체험 BEST</p>
+        </div>
+      </div>
+
+      {/* main영역 */}
+      <div className={styles.mainContainer}>
+        <Dropdown
+          options={['최신순', '낮은가격순', '높은가격순']}
+          selected={selectedSort}
+          onChange={setSelectedSort}
+        />
+      </div>
+      {/* <Footer /> */}
     </>
   );
 }

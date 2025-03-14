0485c98d78744b440f89e1838b3751727a5816fe
Namgyeon <getam101@naver.com>
2025-03-14 21:22:27 +0900

    feat: 회원가입 api 구현

--- src/lib/auth-api.ts
+++ src/lib/auth-api.ts
@@ -1,3 +1,4 @@
+import { AxiosError } from 'axios';
 import instance from './api';
 
 // 회원가입 api
@@ -10,8 +11,17 @@ interface NewUser {
 export async function signUp(newUser: NewUser) {
   try {
     const response = await instance.post('/users', newUser);
+    //추후삭제
     console.log('회원가입 성공:', response.data);
   } catch (error) {
-    console.error('회원가입 오류:', error);
+    if (error instanceof AxiosError) {
+      console.error('회원가입 오류:', error.response?.data || error.message);
+      throw new Error(
+        error.response?.data?.message || '회원가입에 실패했습니다.',
+      );
+    } else {
+      console.error('회원가입 오류:', error);
+      throw error;
+    }
   }
 }

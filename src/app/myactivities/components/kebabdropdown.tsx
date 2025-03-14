74753122fbefe8757c0d9f5be6268f72c0c9418e
Kate Jang <bobaejang1@gmail.com>
2025-03-14 12:36:36 +0900

    fix: CustomDropdown 파일 생성

--- src/app/myactivities/components/kebabdropdown.tsx
+++ src/app/myactivities/components/kebabdropdown.tsx
@@ -4,23 +4,9 @@ import { MoreVertical } from 'lucide-react';
 import CustomDropdown from './customDropdown';
 import { useState, useCallback } from 'react';
 import styles from './customDropdown.module.css';
-import ModalType1 from './ModalType1';
-import useDeleteMyActivity from '@/hooks/useDeleteActivities';
 
-interface KebabDropdownProps {
-  activityId: number;
-}
-
-export default function KebabDropdown({ activityId }: KebabDropdownProps) {
+export default function KebabDropdown() {
   const [isOpen, setIsOpen] = useState(false);
-  const [showModal, setShowModal] = useState(false);
-
-  const { mutate: deleteActivity } = useDeleteMyActivity();
-
-  const handleRemoveActivity = () => {
-    deleteActivity(activityId); // activityId 넘겨줌
-    setShowModal(false);
-  };
 
   const handleCloseDropdown = useCallback(() => {
     setIsOpen(false);
@@ -43,22 +29,13 @@ export default function KebabDropdown({ activityId }: KebabDropdownProps) {
                 if (value === '수정하기') {
                   console.log('수정하기 클릭!');
                 } else if (value === '삭제하기') {
-                  setShowModal(true);
+                  // 삭제하기 모달창 팝업
                 }
               }}
             ></CustomDropdown>
           </div>
         )}
       </div>
-
-      {showModal && (
-        <ModalType1
-          isModalMessage='체험을 삭제하시겠습니까?'
-          showModal={showModal}
-          setShowModal={setShowModal}
-          onConfirm={handleRemoveActivity}
-        />
-      )}
     </div>
   );
 }

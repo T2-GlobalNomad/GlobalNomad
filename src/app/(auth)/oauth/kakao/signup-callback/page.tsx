b6cb45c31316e6e0568ce29746905b0366217f97
Namgyeon <getam101@naver.com>
2025-03-31 15:16:53 +0900

    feat: 카카오 회원가입시 닉네임 폼 모달로 수정

--- src/app/(auth)/oauth/kakao/signup-callback/page.tsx
+++ src/app/(auth)/oauth/kakao/signup-callback/page.tsx
@@ -1,8 +1,12 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
+import Modal from 'react-modal';
 import KakaoSignUpForm from './components/KakaoSignUpForm';
+import toast from 'react-hot-toast';
+
+Modal.setAppElement('body'); // 접근성(A11Y)을 위한 설정 (Next.js 기본 루트 엘리먼트)
 
 export default function KakaoSignUpCallbackPage() {
   const searchParams = useSearchParams();
@@ -10,19 +14,40 @@ export default function KakaoSignUpCallbackPage() {
   const code = searchParams.get('code');
   const redirectUri = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_SIGNUP_URI;
 
+  const [modaleIsOpen, setModalIsOpen] = useState(true);
+
   useEffect(() => {
     if (!code) {
-      console.error('인가 코드가 없습니다.');
-      return;
+      toast.error('인가 코드가 없습니다.');
+      setTimeout(() => {
+        router.push('/signup');
+      }, 1500);
+      setModalIsOpen(false);
     }
   }, [code, router]);
 
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <KakaoSignUpForm
-        code={code as string}
-        redirectUri={redirectUri as string}
-      />
+      <Modal
+        isOpen={modaleIsOpen}
+        shouldCloseOnOverlayClick={false}
+        onRequestClose={() => setModalIsOpen(false)}
+        contentLabel='카카오 회원가입'
+        style={{
+          overlay: { backgroundColor: 'rgba(0, 0, 0, 0.5)' },
+          content: {
+            maxWidth: '400px',
+            maxHeight: '600px',
+            margin: '100px auto',
+            padding: '20px',
+          },
+        }}
+      >
+        <KakaoSignUpForm
+          code={code as string}
+          redirectUri={redirectUri as string}
+        />
+      </Modal>
     </div>
   );
 }

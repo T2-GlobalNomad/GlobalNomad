3d92acb9e3d1581f31bd0676f40855e96a2e8623
HyeyeonJung <jhy20306@gmail.com>
2025-04-01 17:21:22 +0900

    feat: 알림창 클릭시 예약 현황 페이지로 이동

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -140,15 +140,15 @@ export interface Reservation {
 
 /* Notification */
 
-type Notifications = {
-  id?: number;
+export interface Notifications {
+  id: number;
   userId?: number;
   teamId?: number;
-  content?: string;
-  createdAt?: string;
+  content: string;
+  createdAt: string;
   updatedAt?: string;
   deletedAt?: string;
-};
+}
 
 export interface Notification {
   notifications: Notifications[];

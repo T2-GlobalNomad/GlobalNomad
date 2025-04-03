53013fe4e4cf45625bd72a9a1e50419a09be4b69
HyeyeonJung <jhy20306@gmail.com>
2025-04-03 14:49:18 +0900

    chore: calendar, profilecard, notiModal loading

--- src/app/mynotification/components/Calendar.tsx
+++ src/app/mynotification/components/Calendar.tsx
@@ -78,11 +78,11 @@ export default function MyNotificationCalendar({
         onClickDay={(date) => !isLoading && onDateClick?.(date)}
         tileDisabled={() => false}
         tileClassName={({ date }) => {
-          if (isLoading) return styles.loadingTile;
+          if (isLoading) return styles.loadingTile; //로딩상태
           const dateString = date.toISOString().split('T')[0];
           const statuses = markedDates[dateString];
           if (statuses) return styles.markedTile;
-          return ''; // 기본 타일
+          return '';
         }}
         tileContent={({ date }) => {
           const dateString = date.toISOString().split('T')[0];
